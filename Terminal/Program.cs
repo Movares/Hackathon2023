4f7318729c135714eac63c084290f44b0f7b9394
Maarten Joosten <maarten.joosten@rhdhv.com>
2023-12-15 16:23:42 +0100

    Move program to solutions

--- Terminal/Program.cs
+++ Terminal/Program.cs
@@ -3,64 +3,6 @@ using Hackathon2023;
 
 namespace Terminal {
 	internal class Program {
-		static void Main(string[] _) {
-			string firstFile = @"C:\Users\924890\source\repos\Hackathon2023\Ontwerp IMX.V500.xml";
-			string secondFile = @"C:\Users\924890\source\repos\Hackathon2023\Uitlever IMX B.V500.xml";
 
-			ShowDuplicatePuics(firstFile, secondFile);
-			ShowDuplicates(firstFile, secondFile, IMXObjects.GetNonIMX, Compare.IsNonBaseObjectWithRefEqual);
-			ShowDuplicates(firstFile, secondFile, IMXObjects.GetNonIMXNonRef, Compare.IsNonBaseWithoutRefEqual);
-		}
-
-		static void ShowDuplicates(string firstFile, string secondFile, Func<ImSpoor, object[]> objectGetter, Func<object, object, bool> comparer) {
-			object[] firstObjects = objectGetter(ImxSerializer.ReadXml(firstFile)!);
-			object[] secondObjects = objectGetter(ImxSerializer.ReadXml(secondFile)!);
-
-			Console.WriteLine($"Objects in first file: {firstObjects.Length}");
-
-			foreach (IGrouping<Type, object> groups in firstObjects.GroupBy(x => x.GetType())) {
-				Console.WriteLine($"\t{groups.Key.Name}: {groups.Count()}");
-			}
-
-			Console.WriteLine($"Objects in second file: {secondObjects.Length}");
-
-			foreach (IGrouping<Type, object> groups in secondObjects.GroupBy(x => x.GetType())) {
-				Console.WriteLine($"\t{groups.Key.Name}: {groups.Count()}");
-			}
-
-			object[] duplicates = firstObjects.Where(firstObject => secondObjects.Any(secondObject => comparer(firstObject, secondObject))).ToArray();
-
-			Console.WriteLine($"Duplicate objects: {duplicates.Length}");
-
-			foreach (IGrouping<Type, object> groups in duplicates.GroupBy(x => x.GetType())) {
-				Console.WriteLine($"\t{groups.Key.Name}: {groups.Count()}");
-			}
-
-			Console.ReadKey();
-
-		}
-
-		static void ShowDuplicatePuics(string firstFile, string secondFile) {
-			string[] duplicatePuics = GetDuplicatePuics(firstFile, secondFile);
-
-			foreach (string puic in duplicatePuics) {
-				Console.WriteLine($"\t{puic}");
-			}
-
-			Console.WriteLine($"Duplicate puics: {duplicatePuics.Length}");
-			Console.ReadKey();
-		}
-
-		static string[] GetDuplicatePuics(string firstFile, string secondFile) {
-			ImSpoor firstModel = ImxSerializer.ReadXml(firstFile)!;
-			ImSpoor secondModel = ImxSerializer.ReadXml(secondFile)!;
-
-			IEnumerable<string> all = IMXObjects.GetPuics(firstModel).Concat(IMXObjects.GetPuics(secondModel));
-
-			return all.GroupBy(x => x)
-				.Where(x => x.Count() > 1)
-				.Select(x => x.Key)
-				.ToArray();
-		}
 	}
 }
